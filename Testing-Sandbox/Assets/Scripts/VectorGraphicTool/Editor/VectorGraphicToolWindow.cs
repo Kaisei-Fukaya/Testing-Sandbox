c39e26d6faece898addfb5bba216d864f8cafc30
Kaisei-Fukaya <kjfukaya@gmail.com>
2023-03-24 14:26:08 +0000

    Added manipulator for moving points

--- Testing-Sandbox/Assets/Scripts/VectorGraphicTool/Editor/VectorGraphicToolWindow.cs
+++ Testing-Sandbox/Assets/Scripts/VectorGraphicTool/Editor/VectorGraphicToolWindow.cs
@@ -9,90 +9,95 @@ using UnityEditor.Experimental.GraphView;
 using SSL.Data;
 using SSL.Data.Utils;
 
-namespace SSL.Graph
+
+public class VectorGraphicToolWindow : EditorWindow
 {
-    public class VectorGraphicToolWindow : EditorWindow
+    StyleSheet style;
+    StyleSheet _toolbarToggleStyles;
+    VisualElement _mainView;
+    public bool inTrainingMode;
+
+    GAGenData _saveData;
+
+    [MenuItem("Window/Vector Editor")]
+    public static void ShowWindow()
     {
-        StyleSheet _variablesStyleSheet;
-        StyleSheet _generateStyleVariables, _trainStyleVariables;
-        StyleSheet _toolbarToggleStyles;
-        GraphicalAssetGraphView _graphView;
-        PreviewBox _previewWindow;
-        VisualElement _mainView;
-        public bool inTrainingMode;
-
-        GAGenData _saveData;
-
-        [MenuItem("Window/Vector Editor")]
-        public static void ShowWindow()
-        {
-            GetWindow<VectorGraphicToolWindow>();
-        }
-
-        private void CreateGUI()
-        {
-            this.titleContent = new GUIContent("Vector Editor");
-            _generateStyleVariables = (StyleSheet)AssetDatabase.LoadAssetAtPath($"{GAGenDataUtils.BasePath}Editor/Assets/UIStyles/GraphicalAssetGeneratorVariablesGenerate.uss", typeof(StyleSheet));
-            _mainView = new VisualElement()
-            {
-                name = "mainView"
-            };
-            _mainView.AddToClassList("root");
-            //mainView.StretchToParentSize();
-            rootVisualElement.Add(_mainView);
-            AddToolbar();
-        }
-
-
-        private void AddToolbar()
-        {
-            Toolbar toolbar = new Toolbar();
-
-            ToolbarButton saveButton = new ToolbarButton()
-            {
-                text = "Save"
-            };
-            saveButton.clicked += Save;
-
-            ToolbarButton saveAsButton = new ToolbarButton()
-            {
-                text = "Save As"
-            };
-            saveAsButton.clicked += SaveAs;
-
-            ToolbarButton loadButton = new ToolbarButton()
-            {
-                text = "Load"
-            };
-            loadButton.clicked += Load;
-
-            ToolbarSpacer spacer1 = new ToolbarSpacer();
-            ToolbarSpacer spacer2 = new ToolbarSpacer();
-            ToolbarSpacer spacer3 = new ToolbarSpacer();
-            toolbar.Add(saveButton);
-            toolbar.Add(spacer1);
-            toolbar.Add(saveAsButton);
-            toolbar.Add(loadButton);
-            toolbar.Add(spacer2);
-            toolbar.Add(spacer3);
-
-            rootVisualElement.Add(toolbar);
-        }
-
-        void Save()
-        {
-
-        }
-
-        void SaveAs()
-        {
-
-        }
-
-        void Load()
-        {
-
-        }
+        GetWindow<VectorGraphicToolWindow>();
     }
 
+    private void CreateGUI()
+    {
+        this.titleContent = new GUIContent("Vector Editor");
+        style = (StyleSheet)AssetDatabase.LoadAssetAtPath($"Assets/Scripts/VectorGraphicTool/Editor/VectorToolStyle.uss", typeof(StyleSheet));
+        _mainView = new VisualElement()
+        {
+            name = "mainView"
+        };
+        _mainView.AddToClassList("root");
+        //mainView.StretchToParentSize();
+        rootVisualElement.Add(_mainView);
+        rootVisualElement.styleSheets.Add(style);
+        AddToolbar();
+        AddCanvas();
+    }
+
+    private void AddToolbar()
+    {
+        Toolbar toolbar = new Toolbar();
+
+        ToolbarButton saveButton = new ToolbarButton()
+        {
+            text = "Save"
+        };
+        saveButton.clicked += Save;
+
+        ToolbarButton saveAsButton = new ToolbarButton()
+        {
+            text = "Save As"
+        };
+        saveAsButton.clicked += SaveAs;
+
+        ToolbarButton loadButton = new ToolbarButton()
+        {
+            text = "Load"
+        };
+        loadButton.clicked += Load;
+
+        ToolbarSpacer spacer1 = new ToolbarSpacer();
+        ToolbarSpacer spacer2 = new ToolbarSpacer();
+        ToolbarSpacer spacer3 = new ToolbarSpacer();
+        toolbar.Add(saveButton);
+        toolbar.Add(spacer1);
+        toolbar.Add(saveAsButton);
+        toolbar.Add(loadButton);
+        toolbar.Add(spacer2);
+        toolbar.Add(spacer3);
+
+        rootVisualElement.Add(toolbar);
+    }
+    private void AddCanvas()
+    {
+        VisualElement canvas = new VisualElement();
+        canvas.AddToClassList("canvas");
+        VisualElement draggableNode = new VisualElement();
+        draggableNode.AddToClassList("draggable-node");
+        draggableNode.AddManipulator(new PointDragger());
+        canvas.Add(draggableNode);
+        rootVisualElement.Add(canvas);
+    }
+
+    void Save()
+    {
+
+    }
+
+    void SaveAs()
+    {
+
+    }
+
+    void Load()
+    {
+
+    }
 }
