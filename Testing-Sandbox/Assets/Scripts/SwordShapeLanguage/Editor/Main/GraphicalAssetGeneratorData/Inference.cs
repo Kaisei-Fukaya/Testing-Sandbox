3be294b0e611c23d1c32247552afe3a8ad52d2d5
Kaisei-Fukaya <kjfukaya@gmail.com>
2023-06-20 10:18:34 +0100

    Back-end implementation
    -Back-end interpolate and random generate code.
    -Resize to 128

--- Testing-Sandbox/Assets/Scripts/SwordShapeLanguage/Editor/Main/GraphicalAssetGeneratorData/Inference.cs
+++ Testing-Sandbox/Assets/Scripts/SwordShapeLanguage/Editor/Main/GraphicalAssetGeneratorData/Inference.cs
@@ -11,15 +11,25 @@ namespace SSL.Data
 {
     public class Inference
     {
-        NNModel _modelAsset;
-        Model _model;
-        IWorker _worker;
+        NNModel _modelAssetEncoder, _modelAssetBottleneck, _modelAssetDecoder;
+        Model _modelEncoder, _modelBottleneck, _modelDecoder;
+        IWorker _workerEncoder, _workerBottleneck, _workerDecoder;
+        System.Random _random;
 
         public Inference()
         {
-            _modelAsset = (NNModel)AssetDatabase.LoadAssetAtPath($"{GAGenDataUtils.BasePath}Editor/Assets/ONNX/model.onnx", typeof(NNModel));
-            _model = ModelLoader.Load(_modelAsset);
-            _worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, _model);
+            _modelAssetEncoder = (NNModel)AssetDatabase.LoadAssetAtPath($"{GAGenDataUtils.BasePath}Editor/Assets/ONNX/model_encoder.onnx", typeof(NNModel));
+            _modelEncoder = ModelLoader.Load(_modelAssetEncoder);
+            _workerEncoder = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, _modelEncoder);
+
+            _modelAssetBottleneck = (NNModel)AssetDatabase.LoadAssetAtPath($"{GAGenDataUtils.BasePath}Editor/Assets/ONNX/model_bottleneck.onnx", typeof(NNModel));
+            _modelBottleneck = ModelLoader.Load(_modelAssetBottleneck);
+            _workerBottleneck = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, _modelBottleneck);
+
+            _modelAssetDecoder = (NNModel)AssetDatabase.LoadAssetAtPath($"{GAGenDataUtils.BasePath}Editor/Assets/ONNX/model_decoder.onnx", typeof(NNModel));
+            _modelDecoder = ModelLoader.Load(_modelAssetDecoder);
+            _workerDecoder = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, _modelDecoder);
+            _random = new System.Random();
         }
 
         //Image to Model
@@ -27,26 +37,37 @@ namespace SSL.Data
         {
             Texture2D image = LoadImage(path);
             Tensor input = new Tensor(image);
-            _worker.Execute(input);
-            //Extract output
-            Tensor result = _worker.PeekOutput();
-            result.Flatten();
-            var output = Tensor2Graph(result);
-            _worker.Dispose();
+            var output = ReconstructInference(input);
             return output;
         }
 
         //Latent generation
         public GAGenData Rand2Model()
         {
-            GAGenData output = ScriptableObject.CreateInstance<GAGenData>();
+            Tensor input = new Tensor(new int[] { 1, 512 });
+            for (int i = 0; i < input.length; i++)
+            {
+                input[i] = (float)_random.NextDouble();
+            }
+            var output = LatentInference(input);
             return output;
         }
 
         //Latent interpolation
-        public GAGenData Interp()
+        public GAGenData Interp(string pathA, string pathB, float t)
         {
-            GAGenData output = ScriptableObject.CreateInstance<GAGenData>();
+            Texture2D imageA = LoadImage(pathA);
+            Texture2D imageB = LoadImage(pathB);
+            Tensor inputA = new Tensor(imageA);
+            Tensor inputB = new Tensor(imageB);
+            Tensor latentA = EncodeInference(inputA);
+            Tensor latentB = EncodeInference(inputB);
+            Tensor latentLerp = new Tensor(new int[] { 1, 512 });
+            for (int i = 0; i < latentLerp.length; i++)
+            {
+                latentLerp[i] = Mathf.Lerp(latentA[i], latentB[i], t);
+            }
+            var output = LatentInference(latentLerp);
             return output;
         }
 
@@ -60,11 +81,47 @@ namespace SSL.Data
                 data = File.ReadAllBytes(path);
                 texture = new Texture2D(2, 2, TextureFormat.BGRA32, false);
                 texture.LoadImage(data);
-                texture.Resize(256, 256);
+                texture.Resize(128, 128);
             }
 
             return texture;
         }
+
+        GAGenData ReconstructInference(Tensor input)
+        {
+            _workerEncoder.Execute(input);
+            Tensor result = _workerEncoder.PeekOutput();
+            _workerBottleneck.Execute(result);
+            result = _workerBottleneck.PeekOutput();
+            _workerDecoder.Execute(result);
+            result = _workerDecoder.PeekOutput();
+
+            result.Flatten();
+            var output = Tensor2Graph(result);
+            _workerEncoder.Dispose();
+            _workerBottleneck.Dispose();
+            _workerDecoder.Dispose();
+            return output;
+        }
+
+        GAGenData LatentInference(Tensor input)
+        {
+            _workerDecoder.Execute(input);
+            Tensor result = _workerDecoder.PeekOutput();
+            result.Flatten();
+            var output = Tensor2Graph(result);
+            _workerDecoder.Dispose();
+            return output;
+        }
+        Tensor EncodeInference(Tensor input)
+        {
+            _workerEncoder.Execute(input);
+            Tensor result = _workerEncoder.PeekOutput();
+            var output = result.DeepCopy();
+            _workerEncoder.Dispose();
+            return output;
+        }
+
 
         GAGenData Tensor2Graph(Tensor result)
         {
@@ -87,6 +144,23 @@ namespace SSL.Data
             GAGenData output = ScriptableObject.CreateInstance<GAGenData>();
             output.Nodes = newNodes;
             return output;
+        }
+
+        Tensor Graph2Tensor(GAGenData data)
+        {
+            Tensor newTensor = new Tensor(new int[] { 1, 72 });
+            var nodes = data.Nodes;
+            for (int i = 0; i < 12; i++)
+            {
+                var currentParams = nodes[i].Settings.parameters;
+                newTensor[i * 12] = currentParams.size.x;
+                newTensor[i * 12 + 1] = currentParams.size.y;
+                newTensor[i * 12 + 2] = currentParams.curveParams.controlPoint.x;
+                newTensor[i * 12 + 3] = currentParams.curveParams.tipOffset.x;
+                newTensor[i * 12 + 4] = currentParams.relativeBackwardTaper;
+                newTensor[i * 12 + 5] = currentParams.relativeForwardTaper;
+            }
+            return newTensor;
         }
     }
 }
